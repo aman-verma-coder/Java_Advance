ca304f459abfdbf0a12397816dcb7f279d9266f3
Aman Verma <9330nama@gmail.com>
2024-10-17 22:14:12 +0530

    _30p1_introToLinkedList.java

---
+++ _30p1_introToLinkedList.java
@@ -0,0 +1,15 @@
+public class _30p1_introToLinkedList {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
