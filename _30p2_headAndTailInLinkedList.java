445f3c4934b5e083ff740757fdb36280df1f0e8b
Aman Verma <9330nama@gmail.com>
2024-10-18 18:19:14 +0530

    _30p2_headAndTailInLinkedList.java

---
+++ _30p2_headAndTailInLinkedList.java
@@ -0,0 +1,27 @@
+public class _30p2_headAndTailInLinkedList {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+
+        public static Node head;
+        public static Node tail;
+
+        // Methods
+        add();
+
+        remove();
+
+        print();
+
+        search();
+    }
+
+    public static void main(String[] args) {
+        _30p2_headAndTailInLinkedList ll = new _30p2_headAndTailInLinkedList();
+    }
+}
