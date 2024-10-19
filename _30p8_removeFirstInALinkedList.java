ebbe155b6e943948fccb20869530d960067e2306
Aman Verma <9330nama@gmail.com>
2024-10-19 22:33:28 +0530

    _30p8_removeFirstInALinkedList.java

---
+++ _30p8_removeFirstInALinkedList.java
@@ -0,0 +1,64 @@
+public class _30p8_removeFirstInALinkedList {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void printLL() {
+        Node temp = head;
+        if (head == null) {
+            System.out.println("LL is empty");
+            return;
+        }
+        while (temp != null) {
+            System.out.print(temp.data + "=>");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public int removeFirst() {
+        if (size == 0) {
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        return val;
+    }
+
+    public static void main(String[] args) {
+        _30p8_removeFirstInALinkedList ll = new _30p8_removeFirstInALinkedList();
+        ll.addFirst(5);
+        ll.addFirst(4);
+        ll.addFirst(3);
+        ll.addFirst(2);
+        ll.addFirst(1);
+        ll.printLL();
+        ll.removeFirst();
+        ll.printLL();
+    }
+}
