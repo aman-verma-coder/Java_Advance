cf76ee32ca71030cc2a163d19ccbd76dac7c4fe5
Aman Verma <9330nama@gmail.com>
2024-10-19 23:14:35 +0530

    _30p9_removeLastInALinkedList.java

---
+++ _30p9_removeLastInALinkedList.java
@@ -0,0 +1,74 @@
+public class _30p9_removeLastInALinkedList {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void printLL() {
+        Node temp = head;
+        if (head == null) {
+            System.out.println("LL is empty");
+            return;
+        }
+        while (temp != null) {
+            System.out.print(temp.data + "=>");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public int removeLast() {
+        if (size == 0) {
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        // previous: i=size-2
+        Node prev = head;
+        for (int i = 0; i < size - 2; i++) {
+            prev = prev.next;
+        }
+        int val = prev.data;
+        prev.next = null;
+        tail = prev;
+        size--;
+        return val;
+    }
+
+    public static void main(String[] args) {
+        _30p9_removeLastInALinkedList ll = new _30p9_removeLastInALinkedList();
+        ll.addFirst(5);
+        ll.addFirst(4);
+        ll.addFirst(3);
+        ll.addFirst(2);
+        ll.addFirst(1);
+        ll.printLL();
+        System.out.println("Size: " + size);
+        ll.removeLast();
+        ll.printLL();
+        System.out.println("Size: " + size);
+    }
+}
